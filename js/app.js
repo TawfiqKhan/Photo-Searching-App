f67e4890bceeaa8e3d64f9c37bac83c5b14e2ac4
special3220 <special3220@yahoo.com>
2020-09-03 08:16:10 -0400

    class change on scroll, back to up added

--- js/app.js
+++ js/app.js
@@ -2,18 +2,16 @@ let imageUrl = [];
 let imageName = [];
 const imageContainer = document.querySelector(".imageContainer");
 const navUl = document.querySelector("#navUl");
-// const navLi = document.querySelectorAll("#navUl > li");
 const mainBody = document.querySelector("#main-body");
-let message = document.querySelector(".notification");
 let cats = document.querySelector("#Dogs")
+var mybutton = document.getElementById("myBtn");
 
-
-smoothScroling()
 // Searching for photos from unsplash
 
 
 //Things that are done
 
+//highlighing section when it is in viewport(using add/remove classlist)
 // Dynamic section adding via search for photos
 // clicking on navigation link on the nav bar makes it smooth scrolling to the section
 
@@ -22,9 +20,6 @@ smoothScroling()
 
 // Things left to do.....
 // back to top button  appearing when scrolling down
-// highlighing section when it is in viewport(using add/remove classlist)
-
-
 
 const searchPhotos = () => {
 	// imageContainer.innerHTML = "";
@@ -41,14 +36,6 @@ const searchPhotos = () => {
 	fetch(url)
 		.then((response) => response.json())
 		.then((datas) =>
-			// {
-			// 	datas.results.forEach((data) => {
-			// 		//creating image hrf and alt description from unsplash returned data
-			// 		//the appending the image (<img />) tage to the DIV
-
-			// 		let image = `<img class = 'responsive' src=${data.urls.regular} alt='${data.alt_description}' />`;
-			// 		imageContainer.innerHTML += image;
-			// 	});
 			{
 				let imageArray = datas.results;
 				for (let i = 0; i < 4; i++) {
@@ -61,14 +48,13 @@ const searchPhotos = () => {
 										<h3 class="card-text">${imageArray[i].alt_description}</h3>
 									</div>
 								`;
-					// let image = `<img class = 'responsive' src=${imageArray[i].urls.regular} alt='${imageArray[i].alt_description}' />`;
 
 					createdSection.innerHTML += imageDiv;
 				}
 			}
 		);
-		showMessage(searchTerm);
 		addNavigation(searchTerm);
+		showMessage(searchTerm);
 };
 
 function isInViewport(elem) {
@@ -83,20 +69,23 @@ function isInViewport(elem) {
 	);
 }
 
-// window.addEventListener("scroll", function(){
-// 	let fetchedImages = document.querySelectorAll('.responsive')
-// 	fetchedImages.forEach((image)=> {
-// 		let imageName = image.getAttribute('alt');
-// 		if(isInViewport(image)) {
-// 			let imageLi = `<li>${imageName}</li>`;
-// 			navUl.innerHTML += imageLi;
-// 		}
-// 	})
-// })
+window.onscroll = function(){
+	scrollFunction()
+	let Sections = document.querySelectorAll('.imageContainer')
+	Sections.forEach((section)=> {
+		if(isInViewport(section)) {
+			section.classList.add("active-section")
+		} else {
+			section.classList.remove("active-section")
+		}
+	})
+}
 
 function showMessage(term) {
+		let message = document.querySelector("#notification");
 		message.innerHTML = `Images of ${term} has been added!!`;
 		setTimeout(function () {
+			console.log(message)
 			message.innerHTML = "";
 				}, 2000);
 		}
@@ -105,39 +94,18 @@ function showMessage(term) {
 function addNavigation(term) {
 	let newLi = `<li><a href="#${term}">${term}</a></li>`
 	navUl.innerHTML += newLi;
+}
+// window.onscroll = function() {scrollFunction()};
 
-	smoothScroling()
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-function clickingTest(e){
-	console.log(e)
-	cats.scrollIntoView({behavior: "smooth", block:"start", inline: "center"})
-}
-
-
-function smoothScroling() {
-	let navLi = document.querySelectorAll("#navUl > li");
-	navLi.forEach(li=> {
-		console.log(typeof(li))
-		addScrolling(li)
-	}
-	)
-}
-
-function addScrolling(li) {
-	li.addEventListener("click", function (e) {
-		//Preventing default clicking behaviour
-		e.preventDefault();
-		// console.log(typeof(li))
-
-		// Selecting the appropriate list item
-		let liSelector = document.querySelector(`#${li.textContent}`);
-
-		// Smooth scrolling to the appropriate section
-		liSelector.scrollIntoView({
-			behavior: "smooth",
-			block: "start",
-			inline: "center",
-		});
-	});
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
